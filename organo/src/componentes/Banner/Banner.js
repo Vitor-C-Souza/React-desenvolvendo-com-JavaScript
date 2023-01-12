9264074113e2f6beccf21bf04cbb10e54ad9cf36
Makinay <vitor_souza97@hotmail.com>
2023-01-11 21:18:57 -0300

    inicio dos estudos

---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import  './Banner.css'
+
+function Banner() {
+    return(
+        <header className="banner">
+            <img src="/image/banner.png" alt="O banner principal da pagina do organo"></img>
+        </header>
+        )
+
+}
+
+export default Banner
