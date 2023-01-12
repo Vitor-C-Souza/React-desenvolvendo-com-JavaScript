89d3d5e32e6173ed81f7cf3d1b869db3977fb36d
Makinay <vitor_souza97@hotmail.com>
2023-01-12 20:49:43 -0300

    tratado as mudanças de estado do programa

--- organo/src/componentes/ListaSuspensa/index.js
+++ organo/src/componentes/ListaSuspensa/index.js
@@ -4,9 +4,12 @@ const ListaSuspensa = (props) => {
     return(
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select required={props.obrigatorio}>
-                {props.itens.map(item => 
-                <option key={item}>{item}</option>)}
+            <select 
+                onChange={evento => props.aoAlterado(evento.target.value)}
+                required={props.obrigatorio}>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
             </select>
         </div>
     )
