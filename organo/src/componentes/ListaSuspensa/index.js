5a3a82ecc051275e3dabd3997a687b9d13d4e876
Makinay <vitor_souza97@hotmail.com>
2023-01-14 00:31:26 -0300

    projeto finalizado e sem bugs

--- organo/src/componentes/ListaSuspensa/index.js
+++ organo/src/componentes/ListaSuspensa/index.js
@@ -6,7 +6,10 @@ const ListaSuspensa = (props) => {
             <label>{props.label}</label>
             <select 
                 onChange={evento => props.aoAlterado(evento.target.value)}
-                required={props.obrigatorio}>
+                required={props.obrigatorio}
+                value={props.valor}>
+                <option value=""></option>
+                
                 {props.itens.map(item => {
                     return <option key={item}>{item}</option>
                 })}
