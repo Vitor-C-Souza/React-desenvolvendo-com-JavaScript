a2972a6232ac696bbcfe5b88bf745e22618698e1
Makinay <vitor_souza97@hotmail.com>
2023-01-14 00:13:13 -0300

    projeto finalizado só falta resolver bugs

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,8 +1,47 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner/';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Programação',
+      corsPrimarias: '#57C278',
+      corsSecundarias: '#D9F7E9'
+    },
+    {
+      nome: 'Front-End',
+      corsPrimarias: '#82CFFA',
+      corsSecundarias: '#E8F8FF'
+    },
+    {
+      nome: 'Data Science',
+      corsPrimarias: '#A6D157',
+      corsSecundarias: '#F0F8E2'
+    },
+    {
+      nome: 'Devops',
+      corsPrimarias: '#E06B69',
+      corsSecundarias: '#FDE7E8'
+    },
+    {
+      nome: 'UX e Design',
+      corsPrimarias: '#DB6EBF',
+      corsSecundarias: '#FAE9F5'
+    },
+    {
+      nome: 'Mobile',
+      corsPrimarias: '#FFBA05',
+      corsSecundarias: '#FFF5D9'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corsPrimarias: '#FF8A29',
+      corsSecundarias: '#FFEEDF'
+    },
+  ]
 
   const [ colaboradores, setColaboradores ] = useState([])
 
@@ -15,8 +54,17 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario 
+        times={times.map( time => time.nome)}
         aoColaboradorCadastrado=
-          {colaborador => aoNovoColaboradorAdicionado(colaborador)}/>   
+          {colaborador => aoNovoColaboradorAdicionado(colaborador)}
+      />
+      {times.map(time => <Time 
+        key={time.nome} 
+        nome={time.nome} 
+        corsPrimarias={time.corsPrimarias} 
+        corsSecundarias={time.corsSecundarias}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}
     </div>
   );
 }
--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -5,15 +5,7 @@ import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = (props) => {
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
+
 
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
@@ -29,6 +21,11 @@ const Formulario = (props) => {
             imagem,
             time
         })
+
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
     }
 
     return(
@@ -58,13 +55,13 @@ const Formulario = (props) => {
                 <ListaSuspensa 
                     obrigatorio={true} 
                     label='Time'
-                    itens={times} 
+                    itens={props.times} 
                     valor={time}
                     aoAlterado={ valor => setTime(valor) }
                 />
                 <Botao 
                     texto='Criar card'
-                />
+                />                
             </form>
         </section>
     )
